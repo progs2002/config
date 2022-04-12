cb246600fbf6b40c5600dc4187dea2f32c3a6668
Pragato Bhattacharjee <39672908+progs2002@users.noreply.github.com>
2022-04-12 23:25:21 +0530

    Add files via upload

---
+++ fix.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+killall xfce4-panel
+xfce4-panel &
+killall plank
+plank &
---
+++ login.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl -X POST http://10.254.254.29/0/up/ -H "text/html" -d "user=pranab_sav&pass=1234&login=Login"
