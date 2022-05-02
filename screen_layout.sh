42ffb31c298c0e114393b2d74fba5fede8b96c91
progs2002 <pragatobhattacharjee@gmail.com>
2022-05-02 22:51:03 +0530

    added new arandr script for lightdm

---
+++ screen_layout.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-D-0 --off --output HDMI-0 --mode 1366x768 --pos 1920x58 --rotate normal --output HDMI-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off
