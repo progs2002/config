ad773562b5c7c3ab32cb20ff5adefdf06a88d382
progs2002 <pragatobhattacharjee@gmail.com>
2022-06-08 23:46:40 +0530

    added the fish configs and tmux

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,9 @@
+set PATH /home/progs/.cargo/bin $PATH
+
 export FZF_DEFAULT_COMMAND='find . \! \( -type d -path ./.git -prune \) \! -type d \! -name '\''*.tags'\''     -printf '\''%P\n'\'
+
 set fzf_fd_opts --hidden --exclude=.git
-
 set -g __fish_git_prompt_color_branch red
-
 set fish_greeting
 if status is-interactive
     # Commands to run in interactive sessions can go here
